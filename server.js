21f5db4ef8cb2faf66b7862071c207cc19e76d44
SrivatsavD <srivatsav.durvasula@gmail.com>
2021-09-13 12:12:12 +0530

    Made few changes

--- server.js
+++ server.js
@@ -31,6 +31,16 @@ app.use('/' , route);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4000 ;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 })
